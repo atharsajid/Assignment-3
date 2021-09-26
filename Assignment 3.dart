604679ae397c5b1fd5bed1b646aa245bf575390e
atharsajd <athersajid620@gmail.com>
2021-09-26 12:42:02 +0500

    updated

--- Assignment 3.dart
+++ Assignment 3.dart
@@ -58,6 +58,70 @@ for (var i in fruits) {
   print(i);  
 }          
 
+(7).
+var a = 5;
+var b =[];
+for (var i =1; i <= 100; i++) {
+  b.add(i);
+}
+for (var x in b) {
+  if (x%a==0) {
+    print(x);
+    
+  }
+}
+
+(8).
+var cel = 1;
+
+var fer = (cel * 9/5)+32;
+print("$cel  Celsius is equal to $fer fahrenheit");
+
+var f = 50;
+var c = (f-32) * 5/9;
+
+print("$f fahrenheit is equal to $c celsius");
+
+
+(9).
+  
+var num1 = 99;
+var num2 = 56;
+var cal = "*";
+
+if(cal =="+"){
+  var a = num1 + num2;
+  print(a);
+}else if(cal == "-"){
+  var b = num1 - num2;
+  print(b);
+
+}
+else if(cal =="*"){
+  var c = num1 * num2;
+  print(c);
+}
+else if(cal =="/"){
+  var d = num1 / num2;
+  print(d);
+}
+else if (cal =="%"){
+  var e = num1 % num2;
+  print(e);
+}
+
+else{
+  print("error");
+}
+
+(10).
+var a = "e";
+if (a=="a"||a=="e"||a=="i"||a=="o"||a=="u") {
+  print("$a is an Vowel Letters");
+} else {
+  print("$a is a not a Vowel Letters");
+}
+
 
 
 
